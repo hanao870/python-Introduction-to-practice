a9bbf6636c51ea28bfab869bd57e736dbe7dfe62
N.Miki <hanackholehole@gmail.com>
2022-05-05 17:04:35 +0900

    [update] 型アノテーションと docstring 追加

--- chapter.12/booksearch_module.py
+++ chapter.12/booksearch_module.py
@@ -1,15 +1,19 @@
 """unit test サンプル."""
-
 import unittest
 
 
-def book_search():
+def book_search() -> dict[None, None]:
+    """単体テスト用関数."""
+    # 任意の処理
     return {}
 
 
 class BookSearchTest(unittest.TestCase):
+    """単体テストクラス."""
+
     # book_search のテストコード
-    def test_book_search(self):
+    def test_book_search(self) -> None:
+        """`book_search` 関数のテスト."""
         self.assertEqual({}, book_search())
 
 
