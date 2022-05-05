94b97bebe77c6f00b2a20122f3264dd275ab70cf
N.Miki <hanackholehole@gmail.com>
2022-05-05 19:02:54 +0900

    [update] buid_url の単体テスト追加

--- chapter.12/sample/tests/test_api.py
+++ chapter.12/sample/tests/test_api.py
@@ -1,1 +1,16 @@
 """サンプルテストモジュール."""
+import unittest
+
+
+class BuildUrlTest(unittest.TestCase):
+    """モジュール `api` のユニットテストクラス."""
+
+    def test_build_url(self) -> None:
+        """`build_url()` のテスト."""
+        from booksearch.api import build_url
+
+        expected = "https://www.googleapis.com/books/v1/volumes?q=python"
+        actual = build_url({"q": "python"})
+
+        # アサーションメソッドの利用
+        self.assertEqual(expected, actual)
