407080481d7ca61fa4d8117b71a32cdb0bfb4dff
N.Miki <hanackholehole@gmail.com>
2022-05-06 18:05:04 +0900

    [udate] モックオブジェクトの patch デコレータ動作追加

--- chapter.12/sample/mock_patch.py
+++ chapter.12/sample/mock_patch.py
@@ -1,9 +1,26 @@
 """モックオブジェクトの patch 動作確認."""
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import booksearch
 
+
+@patch("booksearch.core.get_books")
+def test_use_mock(mock_get_book: Mock) -> None:
+    """`patch` のデコレータ使用例.
+
+    Args:
+        mock_get_book (Mock): モックオブジェクト
+
+          関数呼び出し時に引数は不要
+    """
+    mock_get_book.return_value = []
+    print(f"{booksearch.core.get_books(q='aaa')=}")
+
+
 if __name__ == "__main__":
+    # patch の第一引数にはドット('.')区切りの文字列を渡す
     with patch("booksearch.core.get_books") as mock_get_book:
         mock_get_book.return_value = []
         print(f"{booksearch.core.get_books(q='s')=}")
+
+    test_use_mock()
