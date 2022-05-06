422593e713a547ded91350104ea5bd91bb409c99
N.Miki <hanackholehole@gmail.com>
2022-05-06 17:38:01 +0900

    [update] モックオブジェクトの呼び出し時の引数チェック追加

--- chapter.12/mock/assertion.py
+++ chapter.12/mock/assertion.py
@@ -1,5 +1,5 @@
 """モックオブジェクトのアサーションメソッド動作確認モジュール."""
-from unittest.mock import Mock
+from unittest.mock import ANY, Mock
 
 
 def assertion_demo() -> None:
@@ -19,6 +19,15 @@ def assertion_demo() -> None:
 
     # 一度だけ呼ばれていることを確認
     mock.assert_called_once()
+
+    # 呼び出され方を確認
+    # 呼び出された時の引数と一致していないのでエラーとなる
+    # mock.assert_called_once_with(1, a=1)
+    # こちらはOK
+    mock.assert_called_once_with(1, a=4)
+
+    # 呼び出し回数は確認せず、一部の引数のみを確認
+    mock.assert_called_with(1, a=ANY)
 
 
 if __name__ == "__main__":
