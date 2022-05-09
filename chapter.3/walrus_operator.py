7381d3663c6822f7e31451dc3da7b3545473e133
N.Miki <hanackholehole@gmail.com>
2022-05-09 20:31:43 +0900

    [update] chapter.3 のモジュールに docstring 追加

--- chapter.3/walrus_operator.py
+++ chapter.3/walrus_operator.py
@@ -4,6 +4,14 @@ from typing import Optional
 
 
 def lottery(goods: list[Optional[str]]) -> str:
+    """`goods` からランダムに選択された文字列を返す.
+
+    Args:
+        goods (list[Optional[str]]): 選択対象のリスト
+
+    Returns:
+        str: `goods` から選択された文字列. `None` の場合は `MISS!` を返す
+    """
     # item への代入が行われる
     if item := random.choice(goods):
         return str(item)
@@ -12,6 +20,7 @@ def lottery(goods: list[Optional[str]]) -> str:
 
 
 def main() -> None:
+    """メイン関数."""
     books = ["notebook", "sketchbook", None, None, None]
     print(lottery(books))
 
